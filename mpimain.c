50f0ef3eaf7cbff800f05a6bb9a4eae7ce5ecdcc
Will Brown <kyrosewoodgang@gmail.com>
2022-03-28 13:27:41 -0400

    [mpimain.c] Move generation in master node to another thread

--- mpimain.c
+++ mpimain.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include <mpi.h>
 #include <gmp.h>
 
@@ -20,6 +21,35 @@ static void invalid_args(const char *arg_type, int world_rank)
 	if (world_rank == 0) fprintf(stderr, "Invalid argument for \"%s\". Check usage with \"--help\".\n", arg_type);
 	MPI_Finalize();
 	exit(1);
+}
+
+void *gen_section_mpi(void *arg)
+{
+	long number = *(long*) arg;
+
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	// Calculate what section of the factorial we want to generate.
+	long start = world_rank * number / world_size;
+	long end = (world_rank + 1) * number / world_size;
+
+	// Start generating factorial
+	mpz_t section;
+	mpz_init(section);
+	gen_factorial_section(section, start, end);
+
+	// Export the result to an array that we can send over MPI
+	size_t buf_size;
+	long *section_buf = mpz_export(NULL, &buf_size, -1, sizeof(long), 0, 0, section);
+	mpz_clear(section);
+	MPI_Send(section_buf, buf_size, MPI_LONG, 0, 1, MPI_COMM_WORLD);
+	free(section_buf);
+
+	return NULL;
 }
 
 int main(int argc, char *argv[])
@@ -54,7 +84,7 @@ int main(int argc, char *argv[])
 	}
 
 	// If the number is too small, act as if we have one node.
-	if (number < world_size * 4) {
+	if (number < world_size * world_size) {
 		if (world_rank == 0) {
 			double start_time = MPI_Wtime();
 			mpz_t result;
@@ -75,12 +105,16 @@ int main(int argc, char *argv[])
 		double start_time = MPI_Wtime();
 
 		// Generate the master node's section of the factorial
+		pthread_t thread;
+		pthread_create(&thread, NULL, gen_section_mpi, &number);
 		mpz_t result;
-		mpz_init(result);
-		gen_factorial_section(result, 0, number / world_size);
+		mpz_init_set_ui(result, 1);
+
+		mpz_t section;
+		mpz_init(section);
 
 		// Get the results from all of the other nodes
-		for (int node = 1; node < world_size; node++) {
+		for (int node = 0; node < world_size; node++) {
 			// Probe for a message so we can get the necessary info to allocate a buffer and recieve it
 			MPI_Status status;
 			int count;
@@ -90,15 +124,14 @@ int main(int argc, char *argv[])
 			MPI_Recv(section_buf, count, MPI_LONG, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 			// Import the array back into an mpz object
-			mpz_t section;
-			mpz_init(section);
 			mpz_import(section, count, -1, sizeof(long), 0, 0, section_buf);
 			free(section_buf);
 
 			// Multiply the recovered section into the final result
 			mpz_mul(result, result, section);
-			mpz_clear(section);
 		}
+
+		mpz_clear(section);
 
 		printf("Successfully generated %ld!\n", number);
 		if (printing) gmp_printf("%Zd\n", result);
@@ -107,21 +140,7 @@ int main(int argc, char *argv[])
 		double end_time = MPI_Wtime();
 		printf("Time to complete: %fs\n", end_time - start_time);
 	} else {
-		// Calculate what section of the factorial we want to generate.
-		long start = world_rank * number / world_size;
-		long end = (world_rank + 1) * number / world_size;
-
-		// Start generating factorial
-		mpz_t section;
-		mpz_init(section);
-		gen_factorial_section(section, start, end);
-
-		// Export the result to an array that we can send over MPI
-		size_t buf_size;
-		long *section_buf = mpz_export(NULL, &buf_size, -1, sizeof(long), 0, 0, section);
-		mpz_clear(section);
-		MPI_Send(section_buf, buf_size, MPI_LONG, 0, 1, MPI_COMM_WORLD);
-		free(section_buf);
+		gen_section_mpi(&number);
 	}
 
 	MPI_Finalize();
