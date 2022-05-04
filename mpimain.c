2c5e6e4a08d9e0eb92e5cd1e9a7a381636ea324d
Will Brown <kyrosewoodgang@gmail.com>
2022-05-04 17:00:51 -0400

    [mpimain.c] Revert completely back to working state

--- mpimain.c
+++ mpimain.c
@@ -9,8 +9,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
-#include <sched.h>
 #include <mpi.h>
 #include <gmp.h>
 
@@ -29,34 +27,6 @@ static void invalid_args(const char *arg_type, int world_rank)
 	if (world_rank == 0) fprintf(stderr, "Invalid argument for \"%s\". Check usage with \"--help\".\n", arg_type);
 	MPI_Finalize();
 	exit(1);
-}
-
-void *gen_section_mpi(void *arg)
-{
-	long number = *(long*) arg;
-
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-	int world_rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-	// Calculate what section of the factorial we want to generate.
-	long start = world_rank * number / world_size;
-	long end = (world_rank + 1) * number / world_size;
-
-	// Start generating factorial
-	mpz_t section;
-	gen_factorial_section(section, start, end);
-
-	// Export the result to an array that we can send over MPI
-	size_t buf_size;
-	long *section_buf = mpz_export(NULL, &buf_size, -1, sizeof(long), 0, 0, section);
-	mpz_clear(section);
-	MPI_Send(section_buf, buf_size, MPI_LONG, 0, 1, MPI_COMM_WORLD);
-	free(section_buf);
-
-	return NULL;
 }
 
 int main(int argc, char *argv[])
@@ -95,6 +65,7 @@ int main(int argc, char *argv[])
 		if (world_rank == 0) {
 			double start_time = MPI_Wtime();
 			mpz_t result;
+			mpz_init(result);
 			gen_factorial(result, number);
 			printf("Successfully generated %ld!\n", number);
 			if (printing) gmp_printf("%Zd\n", result);
@@ -111,41 +82,30 @@ int main(int argc, char *argv[])
 		double start_time = MPI_Wtime();
 
 		// Generate the master node's section of the factorial
-		pthread_t thread;
-		pthread_create(&thread, NULL, gen_section_mpi, &number);
-
 		mpz_t result;
-		mpz_init_set_ui(result, 1);
+		mpz_init(result);
+		gen_factorial_section(result, 0, number / world_size);
 
-		mpz_t section;
-		mpz_init(section);
-
-		// Get the results from all of the nodes
-		for (int node = 0; node < world_size; node++) {
+		// Get the results from all of the other nodes
+		for (int node = 1; node < world_size; node++) {
 			// Probe for a message so we can get the necessary info to allocate a buffer and recieve it
 			MPI_Status status;
 			int count;
 			MPI_Probe(MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
 			MPI_Get_count(&status, MPI_LONG, &count);
 			long *section_buf = malloc(count * sizeof(long));
-			/* MPI_Recv may deadlock when process 0 is trying to obtain data from itself.
-			 * I have read the MPI standard to try and fix this properly, with no success.
-			 * I have tried using my own buffer, this does not work.
-			 * I have tried using nonblocking sends, this does not work.
-			 * However, moving this thread to the end of the scheduler queue somehow stops the deadlock.
-			 * I wish this was a joke. I have spent multiple hours on this. */
-			sched_yield();
 			MPI_Recv(section_buf, count, MPI_LONG, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
 			// Import the array back into an mpz object
+			mpz_t section;
+			mpz_init(section);
 			mpz_import(section, count, -1, sizeof(long), 0, 0, section_buf);
 			free(section_buf);
 
 			// Multiply the recovered section into the final result
 			mpz_mul(result, result, section);
+			mpz_clear(section);
 		}
-
-		mpz_clear(section);
 
 		printf("Successfully generated %ld!\n", number);
 		if (printing) gmp_printf("%Zd\n", result);
@@ -154,7 +114,21 @@ int main(int argc, char *argv[])
 		double end_time = MPI_Wtime();
 		printf("Time to complete: %fs\n", end_time - start_time);
 	} else {
-		gen_section_mpi(&number);
+		// Calculate what section of the factorial we want to generate.
+		long start = world_rank * number / world_size;
+		long end = (world_rank + 1) * number / world_size;
+
+		// Start generating factorial
+		mpz_t section;
+		mpz_init(section);
+		gen_factorial_section(section, start, end);
+
+		// Export the result to an array that we can send over MPI
+		size_t buf_size;
+		long *section_buf = mpz_export(NULL, &buf_size, -1, sizeof(long), 0, 0, section);
+		mpz_clear(section);
+		MPI_Send(section_buf, buf_size, MPI_LONG, 0, 1, MPI_COMM_WORLD);
+		free(section_buf);
 	}
 
 	MPI_Finalize();
